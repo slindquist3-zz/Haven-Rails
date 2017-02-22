5cc156194d478168222cd92c31167043643cfd99
slindquist3 <slindquist3@gmail.com>
2017-02-22 14:17:20 -0500

    Commented out Cors stuff in Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,26 @@
 class ApplicationController < ActionController::API
  before_action :authenticate_user
- before_filter :cors_preflight_check
- after_filter :cors_set_access_control_headers
+ # before_filter :cors_preflight_check
+ # after_filter :cors_set_access_control_headers
 
- def cors_set_access_control_headers
-   headers['Access-Control-Allow-Origin'] = '*'
-   headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-   headers['Access-Control-Request-Method'] = '*'
-   headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-
- end
-
- def cors_preflight_check
-   if request.method == 'OPTIONS'
-     headers['Access-Control-Allow-Origin'] = '*'
-     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-     headers['Access-Control-Request-Method'] = '*'
-     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-
-     render :text => '', :content_type => 'text/plain'
-   end
- end
+ # def cors_set_access_control_headers
+ #   headers['Access-Control-Allow-Origin'] = '*'
+ #   headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+ #   headers['Access-Control-Request-Method'] = '*'
+ #   headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+ #
+ # end
+ #
+ # def cors_preflight_check
+ #   if request.method == 'OPTIONS'
+ #     headers['Access-Control-Allow-Origin'] = '*'
+ #     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+ #     headers['Access-Control-Request-Method'] = '*'
+ #     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+ #
+ #     render :text => '', :content_type => 'text/plain'
+ #   end
+ # end
 
  def authenticate_user
    render json: {error: "unauthorized user!!!"} unless signed_in?
