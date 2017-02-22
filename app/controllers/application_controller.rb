31c1afe9623e316283d9cb9901ac83ffc369726e
slindquist3 <slindquist3@gmail.com>
2017-02-22 14:37:49 -0500

    Changed http to https in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ def set_headers
   if request.headers["HTTPS_ORIGIN"]
   # better way check origin
   # if request.headers["HTTP_ORIGIN"] && /^https?:\/\/(.*)\.some\.site\.com$/i.match(request.headers["HTTP_ORIGIN"])
-    headers['Access-Control-Allow-Origin'] = request.headers["HTTP_ORIGIN"]
+    headers['Access-Control-Allow-Origin'] = request.headers["HTTPS_ORIGIN"]
     headers['Access-Control-Expose-Headers'] = 'ETag'
     headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
     headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match,Auth-User-Token'
