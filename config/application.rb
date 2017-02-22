df3fe7b3e02a43e1e3f58d115f5965be17e9a9ac
slindquist3 <slindquist3@gmail.com>
2017-02-22 15:11:26 -0500

    fixed?

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,7 @@ module YouAndMe
     config.middleware.insert_before 0, Rack::Cors do
      allow do
        origins '*'
-       resource '*', :headers => :any, :methods => [:get, :post, :patch, :put, :delete, :options]
-     end
+       resource '*', :headers => :any, :methods => :any
    end
 
      config.autoload_paths << Rails.root.join('lib')
