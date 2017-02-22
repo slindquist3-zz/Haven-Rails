e6fac0b5be4b35c274e02b73d813d63ff3ad5ee8
slindquist3 <slindquist3@gmail.com>
2017-02-22 15:01:38 -0500

    Added rails-api gem and more stuff to appliaction.rb

--- config/application.rb
+++ config/application.rb
@@ -19,13 +19,11 @@ Bundler.require(*Rails.groups)
 module YouAndMe
   class Application < Rails::Application
 
-    config.middleware.insert_before 0, "Rack::Cors" do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => :any
-      end
-    end
-    
+    config.action_dispatch.default_headers.merge!({
+    'Access-Control-Allow-Origin' => '*',
+    'Access-Control-Request-Method' => '*'
+    })
+
      config.autoload_paths << Rails.root.join('lib')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
