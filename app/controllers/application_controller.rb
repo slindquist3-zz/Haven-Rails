7baa6ada1984940b91da74d333334a3eaeb12b32
slindquist3 <slindquist3@gmail.com>
2017-02-22 14:33:26 -0500

    Added different headers to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
 class ApplicationController < ActionController::API
  before_action :authenticate_user
+ before_filter :set_headers
+
+private
+
+def set_headers
+  if request.headers["HTTP_ORIGIN"]
+  # better way check origin
+  # if request.headers["HTTP_ORIGIN"] && /^https?:\/\/(.*)\.some\.site\.com$/i.match(request.headers["HTTP_ORIGIN"])
+    headers['Access-Control-Allow-Origin'] = request.headers["HTTP_ORIGIN"]
+    headers['Access-Control-Expose-Headers'] = 'ETag'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+    headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match,Auth-User-Token'
+    headers['Access-Control-Max-Age'] = '86400'
+    headers['Access-Control-Allow-Credentials'] = 'true'
+    end
+  end
+
+
  # before_filter :cors_preflight_check
  # after_filter :cors_set_access_control_headers
 
